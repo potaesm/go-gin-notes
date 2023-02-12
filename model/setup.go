ea1084675b3f7e380719213f644547c165ea4908
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2023-02-12 23:53:29 +0700

    Add mysql Docker

--- model/setup.go
+++ model/setup.go
@@ -8,6 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	// docker run --name mysql-standalone -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=test -e MYSQL_USER=user -e MYSQL_PASSWORD=password -p 3306:3306 -d mysql:5.6
 	database, err := gorm.Open(mysql.Open("user:password@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true"), &gorm.Config{})
 
 	if err != nil {
